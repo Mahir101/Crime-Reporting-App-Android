5e28b5569be37883d871a457655f56f2b803f7e8
Md. Mahir Labib <mdmahirlabib@gmail.com>
2019-11-22 14:19:48 +0600

    Added postdetail

--- app/src/main/java/com/example/crimereporter/MainActivity.java
+++ app/src/main/java/com/example/crimereporter/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.crimereporter;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import androidx.core.view.MenuItemCompat;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,15 +14,22 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
-import android.content.Intent;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,11 +41,13 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar mainToolbar;
     private FirebaseAuth fireAuth;
+    private String TAG="tagkori";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //toolbar er jonne
         mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
@@ -123,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
                                 Drawable mDrawable = mImageView.getDrawable();
                                 Bitmap mBitmap = ((BitmapDrawable) mDrawable).getBitmap();
 
-/*                                //pass this data to new activity
+                                //pass this data to new activity
                                 Intent intent = new Intent(view.getContext(), PostDetailActivity.class);
                                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                                 mBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
@@ -132,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
                                 intent.putExtra("title", mTitle); // put title
                                 intent.putExtra("description", mDesc); //put description
                                 startActivity(intent); //start activity
-                             */
+
 
                             }
 
@@ -192,14 +195,17 @@ public class MainActivity extends AppCompatActivity {
                                 Bitmap mBitmap = ((BitmapDrawable) mDrawable).getBitmap();
 
                                 //pass this data to new activity
-                              /*  Intent intent = new Intent(view.getContext(), PostDetailActivity.class);
+                                Intent intentx = new Intent(view.getContext(), PostDetailActivity.class);
                                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                                 mBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
                                 byte[] bytes = stream.toByteArray();
-                                intent.putExtra("image", bytes); //put bitmap image as array of bytes
-                                intent.putExtra("title", mTitle); // put title
-                                intent.putExtra("description", mDesc); //put description
-                                startActivity(intent); //start activity*/
+                                intentx.putExtra("image", bytes); //put bitmap image as array of bytes
+                                Log.d(TAG,bytes.toString());
+                                intentx.putExtra("title", mTitle); // put title
+                                Log.d(TAG,mTitle);
+                                intentx.putExtra("description", mDesc); //put description
+                                Log.d(TAG,mDesc);
+                                startActivity(intentx);
 
 
                             }
@@ -329,253 +335,3 @@ public class MainActivity extends AppCompatActivity {
         sendtoLogin();
     }
 }
-/*
-
-
-
-public class PostsListActivity extends AppCompatActivity {
-
-    LinearLayoutManager mLayoutManager; //for sorting
-    SharedPreferences mSharedPref; //for saving sort settings
-    RecyclerView mRecyclerView;
-    FirebaseDatabase mFirebaseDatabase;
-    DatabaseReference mRef;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_posts_list);
-
-        //Actionbar
-        ActionBar actionBar = getSupportActionBar();
-        //set title
-        mSharedPref = getSharedPreferences("SortSettings", MODE_PRIVATE);
-        String mSorting = mSharedPref.getString("Sort", "newest"); //where if no settingsis selected newest will be default
-
-        if (mSorting.equals("newest")) {
-            mLayoutManager = new LinearLayoutManager(this);
-            //this will load the items from bottom means newest first
-            mLayoutManager.setReverseLayout(true);
-            mLayoutManager.setStackFromEnd(true);
-        } else if (mSorting.equals("oldest")) {
-            mLayoutManager = new LinearLayoutManager(this);
-            //this will load the items from bottom means oldest first
-            mLayoutManager.setReverseLayout(false);
-            mLayoutManager.setStackFromEnd(false);
-        }
-
-        //RecyclerView
-        mRecyclerView = findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-
-        //set layout as LinearLayout
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        //send Query to FirebaseDatabase
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mRef = mFirebaseDatabase.getReference("Data");
-    }
-
-    //search data
-    private void firebaseSearch(String searchText) {
-
-        //convert string entered in SearchView to lowercase
-        String query = searchText.toLowerCase();
-
-        Query firebaseSearchQuery = mRef.orderByChild("search").startAt(query).endAt(query + "\uf8ff");
-
-        FirebaseRecyclerAdapter<Post, ViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<Post, ViewHolder>(
-                        Post.class,
-                        R.layout.row,
-                        ViewHolder.class,
-                        firebaseSearchQuery
-                ) {
-                    @Override
-                    protected void populateViewHolder(ViewHolder viewHolder, Post Post, int position) {
-                        viewHolder.setDetails(getApplicationContext(), Post.getTitle(), Post.getDescription(), Post.getImage());
-                    }
-
-                    @Override
-                    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-                        ViewHolder viewHolder = super.onCreateViewHolder(parent, viewType);
-
-                        viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
-                            @Override
-                            public void onItemClick(View view, int position) {
-                                //Views
-                                TextView mTitleTv = view.findViewById(R.id.rTitleTv);
-                                TextView mDescTv = view.findViewById(R.id.rDescriptionTv);
-                                ImageView mImageView = view.findViewById(R.id.rImageView);
-                                //get data from views
-                                String mTitle = mTitleTv.getText().toString();
-                                String mDesc = mDescTv.getText().toString();
-                                Drawable mDrawable = mImageView.getDrawable();
-                                Bitmap mBitmap = ((BitmapDrawable) mDrawable).getBitmap();
-
-                                //pass this data to new activity
-                                Intent intent = new Intent(view.getContext(), PostDetailActivity.class);
-                                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                                mBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                                byte[] bytes = stream.toByteArray();
-                                intent.putExtra("image", bytes); //put bitmap image as array of bytes
-                                intent.putExtra("title", mTitle); // put title
-                                intent.putExtra("description", mDesc); //put description
-                                startActivity(intent); //start activity
-
-                            }
-
-                            @Override
-                            public void onItemLongClick(View view, int position) {
-                                //TODO do your own implementaion on long item click
-                            }
-                        });
-
-                        return viewHolder;
-                    }
-
-
-                };
-
-        //set adapter to recyclerview
-        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
-    }
-
-
-    //load data into recycler view onStart
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseRecyclerAdapter<Post, ViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<Post, ViewHolder>(
-                        Post.class,
-                        R.layout.row,
-                        ViewHolder.class,
-                        mRef
-                ) {
-                    @Override
-                    protected void populateViewHolder(ViewHolder viewHolder, Post Post, int position) {
-                        viewHolder.setDetails(getApplicationContext(), Post.getTitle(), Post.getDescription(), Post.getImage());
-                    }
-
-                    @Override
-                    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-                        ViewHolder viewHolder = super.onCreateViewHolder(parent, viewType);
-
-                        viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
-                            @Override
-                            public void onItemClick(View view, int position) {
-                                //Views
-                                TextView mTitleTv = view.findViewById(R.id.rTitleTv);
-                                TextView mDescTv = view.findViewById(R.id.rDescriptionTv);
-                                ImageView mImageView = view.findViewById(R.id.rImageView);
-                                //get data from views
-                                String mTitle = mTitleTv.getText().toString();
-                                String mDesc = mDescTv.getText().toString();
-                                Drawable mDrawable = mImageView.getDrawable();
-                                Bitmap mBitmap = ((BitmapDrawable) mDrawable).getBitmap();
-
-                                //pass this data to new activity
-                                Intent intent = new Intent(view.getContext(), PostDetailActivity.class);
-                                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                                mBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                                byte[] bytes = stream.toByteArray();
-                                intent.putExtra("image", bytes); //put bitmap image as array of bytes
-                                intent.putExtra("title", mTitle); // put title
-                                intent.putExtra("description", mDesc); //put description
-                                startActivity(intent); //start activity
-
-
-                            }
-
-                            @Override
-                            public void onItemLongClick(View view, int position) {
-                                //TODO do your own implementaion on long item click
-                            }
-                        });
-
-                        return viewHolder;
-                    }
-
-                };
-
-        //set adapter to recyclerview
-        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //inflate the menu; this adds items to the action bar if it present
-        getMenuInflater().inflate(R.menu.menu, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                firebaseSearch(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //Filter as you type
-                firebaseSearch(newText);
-                return false;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //handle other action bar item clicks here
-        if (id == R.id.action_sort) {
-            //display alert dialog to choose sorting
-            showSortDialog();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void showSortDialog() {
-        //options to display in dialog
-        String[] sortOptions = {" Newest", " Oldest"};
-        //create alert dialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Sort by") //set title
-                .setIcon(R.drawable.ic_action_sort) //set icon
-                .setItems(sortOptions, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // The 'which' argument contains the index position of the selected item
-                        // 0 means "Newest" and 1 means "oldest"
-                        if (which == 0) {
-                            //sort by newest
-                            //Edit our shared preferences
-                            SharedPreferences.Editor editor = mSharedPref.edit();
-                            editor.putString("Sort", "newest"); //where 'Sort' is key & 'newest' is value
-                            editor.apply(); // apply/save the value in our shared preferences
-                            recreate(); //restart activity to take effect
-                        } else if (which == 1) {
-                            {
-                                //sort by oldest
-                                //Edit our shared preferences
-                                SharedPreferences.Editor editor = mSharedPref.edit();
-                                editor.putString("Sort", "oldest"); //where 'Sort' is key & 'oldest' is value
-                                editor.apply(); // apply/save the value in our shared preferences
-                                recreate(); //restart activity to take effect
-                            }
-                        }
-                    }
-                });
-        builder.show();
-    }
-
-}
-* */
