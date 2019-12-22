b4c8707caeb666eab8c706ab290a90f1adbee11f
Md. Mahir Labib <mdmahirlabib@gmail.com>
2019-12-23 04:30:22 +0600

    UIACTIVITY ADDED

---
+++ app/src/main/java/com/example/crimereporter/UIActivity.java
@@ -0,0 +1,63 @@
+package com.example.crimereporter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui);
+
+
+
+
+    }
+
+    public void s1(View view) {
+        Intent intent = new Intent(UIActivity.this, Main2Activity.class);
+
+        startActivity(intent);
+        finish();
+    }
+    public void s2(View view) {
+        Intent intent = new Intent(UIActivity.this, MainActivity.class);
+
+        startActivity(intent);
+        finish();
+    }
+    public void s3(View view) {
+        Intent intent = new Intent(UIActivity.this, PostActivity.class);
+
+        startActivity(intent);
+        finish();
+    }
+    public void s4(View view) {
+        Intent intent = new Intent(UIActivity.this, MissingActivity.class);
+
+        startActivity(intent);
+        finish();
+    }
+    public void s5(View view) {
+        Intent intent = new Intent(UIActivity.this, WebActivity.class);
+
+        startActivity(intent);
+        finish();
+    }
+    public void s6(View view) {
+        Intent intent = new Intent(UIActivity.this, ProfileActivity.class);
+
+        startActivity(intent);
+        finish();
+    }
+
+
+
+
+
+
+}
